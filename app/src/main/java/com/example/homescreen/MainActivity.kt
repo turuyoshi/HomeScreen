dd55b4ab0929802289c52b4bcf22fb067078a8ad
arthur <arthur.tinen@gmail.com>
2024-11-16 14:50:49 -0300

    Initial commit

---
+++ app/src/main/java/com/example/homescreen/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.homescreen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.homescreen.databinding.ActivityMainBinding
+import java.util.zip.Inflater
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var  navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavigation()
+    }
+
+    private fun initNavigation(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.btnv, navController)
+
+    }
+}
---
+++ app/src/main/java/com/example/homescreen/PostAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.homescreen
+
+class PostAdapter {
+}
